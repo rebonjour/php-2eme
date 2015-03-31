8aa25872dc6adc15612a9c5108d7ad991a4f8f75
Rebecca Broye <Rebecca.broye@cpnv.ch>
2015-03-31 16:38:58 +0200

    ex.3

---
+++ ch02/ex03/get.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <title>On s'en fou</title>
+    <meta charset="utf-8">
+    <style></style>
+  </head>
+  <body>
+    <a href="./traite.php?nom=Rebecca">Appuyez ici</a> pour transmettre le nom<br>
+  </body>
+</html>
---
+++ ch02/ex03/post.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Inscris-toi!</title>
+    <meta charset="utf-8" >
+  </head>
+  <body>
+    <form action="./traite.php"  method="post">
+      <fieldset>
+        <label>Nom :<br>
+          <input type="text" name="nom">
+        </label><br>
+
+        <input type="submit" name="submit" value=" Envoyer ">
+				
+			</fieldset>
+		</form>
+	</body>
+</html> 
